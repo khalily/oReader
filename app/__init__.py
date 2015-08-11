2942f73ffe14111d6f66882217956a1e9321d243
khalily <boywyang@gmail.com>
2015-08-11 23:40:36 +0800

    add login for server

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
+from flask.ext.triangle import Triangle
 
 from config import config
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
+
+triangle = Triangle()
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -15,11 +18,15 @@ def create_app(config_name):
 
     db.init_app(app)
     login_manager.init_app(app)
+    triangle.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     from .api import api as api_blueprint
     app.register_blueprint(api_blueprint, url_prefix='/api')
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
--- app/static/js/site.js
+++ app/static/js/site.js
@@ -1,10 +1,19 @@
-function login() {
 
-}
 
 var rssApp = angular.module('rssApp', []);
 
-rssApp.controller('rssController', ['$scope', function ($scope) {
+rssApp.controller('rssController', ['$scope', '$http', function ($scope, $http) {
+    $scope.login = function login(user) {
+        if (user.email) {
+            console.log("test login")
+        }
+        $http.post('/auth/login', {email: user.email}).
+            then(function(response) {
+                console.log(response.data)
+            }, function(error) {
+                console.log(error.data)
+        })
+    };
     $scope.stories = [];
 }]);
 
