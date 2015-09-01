95f010308f71f1c7e50215f369d692b5fcf96e06
khalily <boywyang@gmail.com>
2015-09-01 16:42:53 +0800

    add favicon support

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,10 @@
 from . import main
-from flask import render_template
+from flask import render_template, redirect, url_for
 
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/favicon.ico')
+def favicon():
+    return redirect(url_for('static', filename='favicon.ico'))
